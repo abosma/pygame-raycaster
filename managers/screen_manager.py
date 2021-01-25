04acc51afdb1238c4e24f66b7e7358001ebb62a9
Atilla Bosma <atillabosma@hotmail.com>
2021-01-25 23:33:37 +0100

    Cleanup old unneccesary code
    -- Removed screen, moved it to the screen_manager class which actually implements the manager interface

--- managers/screen_manager.py
+++ managers/screen_manager.py
@@ -1,9 +1,13 @@
+from config import SCREEN_HEIGHT, SCREEN_WIDTH
 from managers.manager import Manager
+
 
 class ScreenManager(Manager):
     def start(self):
-        print("test start")
+        self.screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_WIDTH), pygame.RESIZABLE)
 
     def update(self, dt: float, to_draw_entites = None):
-        return
-        #print(f"test update {self}")
+        for entity in to_draw_entites:
+            self.screen.blits(entity.surf, entity.rect)
+        
+        pygame.display.flip()
